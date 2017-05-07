56dbba808e371d0c8a7227bcd7026bcd4222627c
Max Taldykin <jorpic@gmail.com>
2017-05-07 14:22:21 +0300

    monitor: gracefully handle unix signals

--- monitor/src/Main.hs
+++ monitor/src/Main.hs
@@ -3,11 +3,13 @@ module Main where
 
 import           Control.Monad (forever, void)
 import           Control.Concurrent (forkIO, threadDelay)
+import           Control.Concurrent.MVar
 
 import           Data.Monoid ((<>))
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.ByteString.Lazy as LB
 import qualified Data.Configurator as Config
 import qualified Data.Aeson as Aeson
 import           Data.Aeson ((.=))
@@ -17,6 +19,7 @@ import           Database.PostgreSQL.Simple.SqlQQ (sql)
 
 import           System.IO (stderr)
 import qualified System.Environment as Env
+import qualified System.Posix.Signals as Signal
 
 import qualified Network.WebSockets as WS
 import qualified Wuss
@@ -37,10 +40,14 @@ main = do
       pgPwd     <- Config.require conf "pg.pass"
       pgDb      <- Config.require conf "pg.db"
 
-      logInfo $ "Connecting to blockchain.info"
-      blockchainInfo
+      -- handle interrupts gracefully
+      interruptFlag <- newEmptyMVar :: IO (MVar ())
+      let handleSig = Signal.Catch $ putMVar interruptFlag ()
+      void $ Signal.installHandler Signal.sigINT handleSig Nothing
+      void $ Signal.installHandler Signal.sigTERM handleSig Nothing
 
-      logInfo $ "Connecting to Postgres on " <> T.pack pgHost
+
+      logInfo $ "Connecting to Postgresql on " <> T.pack pgHost
       let cInfo = PG.ConnectInfo
             pgHost pgPort
             pgUser pgPwd
@@ -51,32 +58,45 @@ main = do
           (fromInteger 20) -- seconds
           5 -- maximum number of resources to keep open
 
-      return ()
+      logInfo $ "Connecting to blockchain.info"
+      blockchainInfo interruptFlag
+
+      readMVar interruptFlag
+      logInfo "Terminating due to signal\n"
+      sleep 3 -- give some time to release resources
 
     _ -> error $ "Usage: " ++ prog ++ " <config.conf>"
 
 
-blockchainInfo :: IO ()
-blockchainInfo = do
+blockchainInfo :: MVar () -> IO ()
+blockchainInfo intFlag = do
   let bcInfo = "ws.blockchain.info"
   Wuss.runSecureClient bcInfo 443 "/inv" $ \conn -> do
-    logInfo "Connected"
+    void . forkIO . forever
+      $ WS.receiveData conn >>= T.putStrLn
 
     void . forkIO . forever $ do
-      WS.receiveData conn >>= T.putStrLn
+      sleep 30
+      WS.sendPing conn $ json ["op" .= txt "ping"]
 
-    WS.sendTextData conn $ Aeson.encode $ Aeson.object
-          [ "op" .= txt "unconfirmed_sub" ]
-    forever $ threadDelay 10000
+    WS.sendTextData conn $ json ["op" .= txt "unconfirmed_sub"]
+
+    readMVar intFlag
+    WS.sendClose conn $ txt "See ya!"
 
 
 -----------
 -- Utility
 -----------
 
+sleep :: Int -> IO ()
+sleep seconds = threadDelay $ seconds * 10^(6 :: Int)
 
 logInfo :: Text -> IO ()
 logInfo = T.hPutStrLn stderr
 
 txt :: Text -> Text
 txt = id
+
+json :: [(Text, Aeson.Value)] -> LB.ByteString
+json = Aeson.encode . Aeson.object
