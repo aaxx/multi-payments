6fcbb74b53333405cc0b74c2e2a68a06c4c30a3e
Max Taldykin <jorpic@gmail.com>
2017-06-13 16:43:08 +0300

    DB: load one-time addresses

--- db/init.sh
+++ db/init.sh
@@ -2,3 +2,6 @@
 
 PSQL="psql -h $RDS_HOSTNAME -U $RDS_USERNAME $RDS_DB_NAME"
 ls *sql | sort | xargs cat | ${PSQL}
+
+cat *.csv | sed 's/;[^;]*$//' \
+  | ${PSQL} -c "copy address from stdin with delimiter as ';'"
