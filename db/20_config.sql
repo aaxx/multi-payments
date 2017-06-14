a0cd7a268944775fc1851d58d7cabda272ab2cb7
Max Taldykin <jorpic@gmail.com>
2017-06-14 19:05:22 +0300

    Adjust SNM price constant

--- db/20_config.sql
+++ db/20_config.sql
@@ -1,19 +1,11 @@
 create type currency_code as enum
-  ( 'BTC'
-  , 'LTC'
-  , 'DASH'
-  , 'XMR'
-  , 'ETH'
-  , 'ETC'
-  , 'TIME'
-  , 'SNM'
-  );
+  ( 'BTC' , 'LTC' , 'DASH' , 'XMR' , 'ETH' , 'ETC' , 'TIME' , 'SNM');
 
 
 create table ico_config
   ( snm_total_limit int8 not null default 444000000
   , snm_ico_limit int8 not null default 331360000
-  , snm_per_eth int8 not null default 2424
+  , snm_per_eth int8 not null default 2824
   , time_address text not null default
     '0x6531f133e6deebe7f2dce5a0441aa7ef330b4e53'
   , time_transfer_topic text not null default
